3a1374f7982eaf59e8d877c080b844b60acf12d4
Nayem-Hasan <nayeemhasanpr@gmail.com>
2025-01-08 01:37:23 +0600

    uncaughtException && unhandledRejection error handled

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ dotenv.config();
 //env variable
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   logger.info(`server is listening at ${PORT} port`);
+});
+
+//handle server errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
