910d99edeca38ab49a513447d98fe76a791a3e0e
Nayeem-Hasan <nayeemhasanpr@gmail.com>
2025-01-08 12:03:44 +0600

    errorHandler added

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
+
 //dotenv config
 dotenv.config();
 
@@ -49,5 +51,24 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("welcome");
 });
+
+//not found page
+app.use(async (req, res, next) => {
+  // res.status(404).json({ message: "This route does not exist." });
+  next(createHttpError.NotFound("This route does not exist."));
+});
+
+// error handling middleware
+function errorHandler(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+}
+
+app.use(errorHandler);
 
 export default app;
